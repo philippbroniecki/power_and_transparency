5c3dfc185e06eedb55187c6bb27b49d6470004b7
philippbroniecki <philippbroniecki@gmail.com>
2019-06-11 15:04:01 +0200

    changed init

--- analysis/scripts/auxiliary/init.R
+++ analysis/scripts/auxiliary/init.R
@@ -1,6 +1,6 @@
 init <- function(){
-  if(!file.exists(paste(main,"work data",sep=""))) dir.create(path=paste(main,"work data",sep="")) 
-  if(!file.exists(paste(main,"tables",sep=""))) dir.create(path=paste(main,"tables",sep=""))
-  if(!file.exists(paste(main,"figures",sep=""))) dir.create(path=paste(main,"figures",sep=""))
+  if(!file.exists(paste(main,"/","work data",sep=""))) dir.create(path=paste(main,"/","work data",sep="")) 
+  if(!file.exists(paste(main,"/","tables",sep=""))) dir.create(path=paste(main,"/","tables",sep=""))
+  if(!file.exists(paste(main,"/","figures",sep=""))) dir.create(path=paste(main,"/","figures",sep=""))
 }
 init()
